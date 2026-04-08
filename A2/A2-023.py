e7137d7d11d154b1f3f935d533c16aef9e3ba2c6
Sarunporn Phiewngam <06604@sukhon.ac.th>
2026-04-08 21:15:19 +0700

    learn

--- A2/A2-023.py
+++ A2/A2-023.py
@@ -1,59 +1,51 @@
-s = input().strip()
-s_up = s.upper()
+r = input().strip()
+r_up = r.upper()
 
-if all(c in 'IT' for c in s_up):
-    print(f"unknown {len(s)}")
-else:
-    max_a = 0
+if all(c in 'TI' for c in r_up):
+    print(f'unknown {len(r)}')
+else :
     curr_a = 0
+    max_a = 0
     is_valid = True
     err_idx = -1
 
-    # 2. วนลูปเช็คทีละตัวอักษร
-    for i in range(len(s_up)):
-        c = s_up[i]
-        
-        # กฎข้อ 0: ตัวอักษรต้องอยู่ใน R, A, B, I, T เท่านั้น
-        if c not in 'RABIT':
-            is_valid = False
-            err_idx = i
-            break
-            
-        prev = s_up[i-1] if i > 0 else ''
+    for i in range(len(r_up)):
+        pres = r_up[i]
 
-        # กฎข้อ 2: หลัง R ต้องเป็น A
-        if prev == 'R' and c != 'A':
-            is_valid = False
-            err_idx = i
-            break
-            
-        # กฎข้อ 4: หลัง B ต้องเป็น I หรือ T
-        if prev == 'B' and c not in 'IT':
-            is_valid = False
-            err_idx = i
-            break
-            
-        # กฎข้อ 3: A ต้องตามหลัง R หรือ A เท่านั้น
-        if c == 'A' and prev not in 'RA':
-            is_valid = False
-            err_idx = i
-            break
-            
-        # กฎข้อ 5: ห้ามลงท้ายด้วย R หรือ B
-        if i == len(s_up) - 1 and c in 'RB':
+        if pres not in 'RABIT':
             is_valid = False
             err_idx = i
             break
 
-        # 3. นับจำนวน A ที่ติดกัน
-        if c == 'A':
+        prev = r_up[i-1] if i > 0 else None
+
+        if prev == 'R' and pres != 'A':
+            is_valid = False
+            err_idx = i
+            break
+
+        if prev == 'B' and pres not in 'IT':
+            is_valid = False
+            err_idx = i
+            break
+
+        if pres == 'A' and prev not in 'RA':
+            is_valid = False
+            err_idx = i
+            break
+
+        if i == len(r_up) - 1 and pres in 'RB':
+            is_valid = False
+            err_idx = i
+            break
+
+        if pres == 'A':
             curr_a += 1
             max_a = max(max_a, curr_a)
         else:
-            curr_a = 0 # รีเซ็ตเมื่อเจอตัวอื่น
+            curr_a = 0
 
-    # 4. แสดงผลลัพธ์
     if is_valid:
-        print(f"yes {max_a}")
+        print(f'yes {max_a}')
     else:
-        print(f"no {err_idx}")
+        print(f'no {err_idx}')
